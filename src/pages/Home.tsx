b5d03843a58f8c1cf1a0c2ad8ee78103ab738348
Maria <mariafacchin00@hotmail.com>
2025-07-20 21:15:14 +0200

    Include language context.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,19 @@ import Extras from '../components/sections/Extras'
 import Projects from '../components/sections/Projects'
 import { content } from '../data/translations'
 import { motion, AnimatePresence } from 'framer-motion'
+import { useLanguage } from '../context/LanguageContext'
 
-interface HomeProps {
-    language: 'en' | 'sv';
-}
 
-function Home({ language }: HomeProps) {
+function Home() {
+    const { language } = useLanguage();
     const { greeting, description, image } = content[language];
     const textColor = language === 'sv' ? 'text-amber-300' : 'text-red-600';
+    
     const fadeIn = {
         initial: { opacity: 0, y: 30 },
         whileInView: { opacity: 1, y: 0 },
-        transition: { duration: 0.5 },
-        viewport: { once: true, amount: 0.3 }
+        transition: { duration: 0.35 },
+        viewport: { once: true, amount: 0.5 }
     };
     
     return (
@@ -30,13 +30,14 @@ function Home({ language }: HomeProps) {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8 }}
                     >
-                    <h2 className={`text-center text-4xl font-semibold ${textColor}`}>
+                    <h2 className={`text-center text-4xl tracking-tighter font-semibold ${textColor}`}>
                         {greeting}
                     </h2>
-                    <p className="mx-auto mt-2 max-w-lg text-center text-xl font-semibold tracking-tight text-balance text-gray-950">
+                    <p className="mx-auto mt-2 max-w-lg tracking-tighter text-center text-xl font-semibold tracking-tight text-balance text-gray-950">
                     {description}
                     </p>
                     <div className="mt-10 flex justify-center">
+                        {/* ADD A NICER IMAGE EFFECT */}
                         <motion.img
                             key={image}
                             src={image}
@@ -45,13 +46,13 @@ function Home({ language }: HomeProps) {
                         />
                     </div>
                     <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-2">
-                        <motion.div {...fadeIn}>
+                        <motion.div {...fadeIn} className='row-span-2'>
                             <About />
                         </motion.div>
-                        <motion.div {...fadeIn}>
+                        <motion.div {...fadeIn} className='flex'>
                             <Experience />
                         </motion.div>
-                        <motion.div {...fadeIn}>
+                        <motion.div {...fadeIn} className='flex'>
                             <Extras />
                         </motion.div>
                     </div>
