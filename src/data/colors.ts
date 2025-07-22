db1a0f199ffab179cf2531f52f1f455f6cbfdcec
Maria <mariafacchin00@hotmail.com>
2025-07-22 21:41:32 +0200

    Create color scheme

---
+++ src/data/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    red: {
+        primary: '#A30015',
+        secondary: '#020887',
+        popupBackground: '#F7F0EE',
+        lightBackground: '#F7F4F3',
+    },
+    blue: {
+        primary: '#F5B700',
+        secondary: '#1C5D99',
+        popupBackground: '#C0D6DF',
+        lightBackground: '#E7ECEF',
+    },
+};
